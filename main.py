02af7b8fc5a01ea5c454dcb923eacebe44baa9c9
WesleyHCode <wesleyh.12000@gmail.com>
2026-09-23 22:40:46 -0400

    a

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ clock = pygame.time.Clock()
 # 🌟 CHANGE 2: Wrap your entire game setup and loop inside an async main function
 async def main():
     # Load your assets exactly the same way inside the function
-    ball_image = pygame.image.load("assets/ball.png")
-    ball_rect = ball_image.get_rect(center=(320, 240))
+    player_image = pygame.image.load("assets/player.png")
+    player_rect = player_image.get_rect(center=(320, 240))
     speed = [5, 4]
     
     # Audio elements load normally here
-    bounce_sound = pygame.mixer.Sound("assets/bounce.wav")
+    bounce_sound = pygame.mixer.Sound("assets/bounce.mp3")
 
     running = True
     while running:
@@ -27,19 +27,19 @@ async def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        ball_rect.x += speed[0]
-        ball_rect.y += speed[1]
+        player_rect.x += speed[0]
+        player_rect.y += speed[1]
 
         # Bounce logic and sound triggers
-        if ball_rect.left < 0 or ball_rect.right > 640:
+        if player_rect.left < 0 or player_rect.right > 640:
             speed[0] = -speed[0]
             bounce_sound.play()
-        if ball_rect.top < 0 or ball_rect.bottom > 480:
+        if player_rect.top < 0 or player_rect.bottom > 480:
             speed[1] = -speed[1]
             bounce_sound.play()
 
         screen.fill((30, 30, 30))
-        screen.blit(ball_image, ball_rect)
+        screen.blit(player_image, player_rect)
         pygame.display.flip()
         
         clock.tick(60)
